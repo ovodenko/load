d2a26ba8ef988c72411242a7ab72f77f65cd4a62
vladimir.ovodenko <vladimir.ovodenko@gett.com>
2021-02-19 18:12:08 +0300

    test for parallel request to API

--- src/test/java/org/load/LoadTests.java
+++ src/test/java/org/load/LoadTests.java
@@ -1,34 +1,64 @@
 package org.load;
 
-import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import org.junit.Test;
 
 public class LoadTests {
+  private static final int MAX_THREAD_COUNT = 5;
+  public static final int MAX_HTTP_REQUEST_NUMBER = 10;
 
   @Test
-  public void checkMeanAndMaxTime() {
+  public void checkMeanAndMaxResponseTimePositive() {
+    final String END_POINT = "https://api.covid19api.com/summary";
+    Worker.setEndpoint(END_POINT);
 
-    Data responseDetails = performRequestToAPI("https://api.covid19api.com/summary", 50);
+    List<Thread> workers = new ArrayList<>();
 
-    assertThat(responseDetails.getAvg()).as("mean delay time").isLessThan(400);
-    assertThat(responseDetails.getMax()).as("max delay time").isLessThan(600);
-  }
-
-
-
-  public Data performRequestToAPI(String endPoint, int requestNumber) {
-    ArrayList<Long> delay = new ArrayList<Long>();
-    double sum = 0;
-
-    for (int i = 0; i < requestNumber; i++) {
-      delay.add(given().get(endPoint).then().extract().time());
-      sum += delay.get(i);
+    for (int threadNumber = 0; threadNumber < MAX_THREAD_COUNT; threadNumber++) {
+      Worker worker = new Worker();
+      Thread thread = new Thread(worker);
+      thread.start();
+      workers.add(thread);
     }
 
-    return new Data(sum / delay.size(), delay.size());
+    for (Thread worker : workers) {
+      try {
+        worker.join();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+
+    List<Data> listResponseDetails = Worker.getListResponseDetails();
+
+    assertMeanAndMaxResponseTime(listResponseDetails, 900, 2500);
+  }
+
+  private static void assertMeanAndMaxResponseTime(
+      List<Data> lstResponseDetails, double expectedAvgMillis, double expectedMaxResponseMillis) {
+    double actualAvgMillis = 0.0d;
+
+    List<Double> maxes = new ArrayList<>();
+
+    for (Data data : lstResponseDetails) {
+      actualAvgMillis = actualAvgMillis + data.getAvg();
+      maxes.add(data.getMax());
+    }
+
+    actualAvgMillis = actualAvgMillis / lstResponseDetails.size();
+    Collections.sort(maxes);
+
+    assertThat(expectedAvgMillis).as("mean delay time").isGreaterThan(actualAvgMillis);
+
+    Double actualMaxResponseMillis = maxes.get(maxes.size() - 1);
+
+    assertThat(expectedMaxResponseMillis).as("max delay time").isGreaterThan(actualMaxResponseMillis);
+
+    System.out.println("Mean response time [ms]: " + actualAvgMillis);
+    System.out.println("Max response time [ms]: " + actualMaxResponseMillis);
   }
 }
